e8247359aa56ae236d1e12651686a4602ceded9a
GabrielSilva13 <jeovabolado67@gmail.com>
2023-04-04 11:33:23 -0300

    Finalizando projeto

---
+++ mobile/src/components/Form/index.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { View, TextInput, Image, Text, TouchableOpacity } from "react-native";
+import { ArrowLeft } from "phosphor-react-native";
+import { captureScreen } from "react-native-view-shot";
+import * as FileSystem from "expo-file-system";
+import { FeedbackTypeProps } from "../../components/Widget";
+import { Button } from "../../components/Button";
+import { Screenshot } from "../../components/Screenshot";
+
+import { styles } from "./styles";
+import { theme } from "../../theme";
+
+import { feedbackTypes } from "../../utils/feedbackTypes";
+import { api } from "../../libs/api";
+
+interface FormProps {
+  feedbackType: FeedbackTypeProps;
+  onFeedbackCanceled: () => void;
+  onFeedbackSent: () => void;
+}
+
+export function Form({
+  feedbackType,
+  onFeedbackCanceled,
+  onFeedbackSent,
+}: FormProps) {
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [comment, setComment] = useState("");
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleScreenshot() {
+    captureScreen({
+      format: "png",
+      quality: 0.8,
+    })
+      .then((uri: string | null) => setScreenshot(uri))
+      .catch((err: any) => console.error(err));
+  }
+
+  function handleScreenshotRemove() {
+    setScreenshot(null);
+  }
+
+  async function handleSendFeedback() {
+    if (isSendingFeedback) {
+      return;
+    }
+
+    setIsSendingFeedback(true);
+    const screenshotBase64 =
+      screenshot &&
+      (await FileSystem.readAsStringAsync(screenshot, { encoding: "base64" }));
+
+    try {
+      await api.post("/feedbacks", {
+        type: feedbackType,
+        comment,
+        screenshot: `data:image/png;base64,${screenshotBase64}`,
+      });
+      onFeedbackSent();
+    } catch (err) {
+      console.error(err);
+      setIsSendingFeedback(false);
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={onFeedbackCanceled}>
+          <ArrowLeft
+            size={24}
+            weight="bold"
+            color={theme.colors.text_secondary}
+          />
+        </TouchableOpacity>
+
+        <View style={styles.titleContainer}>
+          <Image source={feedbackTypeInfo.image} style={styles.image} />
+          <Text style={styles.titleText}>{feedbackTypeInfo.title}</Text>
+        </View>
+      </View>
+
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder="Algo não está funcionando bem ? Queremos corrigir. Conte com detalhes o que está acontecendo..."
+        placeholderTextColor={theme.colors.text_secondary}
+        onChangeText={setComment}
+      />
+
+      <View style={styles.footer}>
+        <Screenshot
+          onTakeShot={handleScreenshot}
+          onRemoveShot={handleScreenshotRemove}
+          screenshot={screenshot}
+        />
+        <Button onPress={handleSendFeedback} isLoading={isSendingFeedback} />
+      </View>
+    </View>
+  );
+}
---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "91b99e87cf47ea",
+    pass: "dd05ab26060b3c",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Gabriel Gonçalves <jeovabolado67@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
